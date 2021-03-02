75b2352b73464947da694611d1a6c99080f02b04
왕영문 <wym6764@gmail.com>
2021-03-02 21:59:44 +0900

    알고리즘 푼 문제들

---
+++ src/divide/Main_2630_색종이.java
@@ -0,0 +1,16 @@
+package divide;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2630_색종이 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	}
+}
---
+++ src/wantam/Solution_1912_연속합.java
@@ -0,0 +1,5 @@
+package wantam;
+
+public class Solution_1912_연속합 {
+
+}
